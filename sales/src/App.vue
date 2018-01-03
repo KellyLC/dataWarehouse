c4963cc372d7d4e1eea64c08337b7258d382ed65
lin <184628769@qq.com>
2018-01-03 20:57:31 +0800

    new

--- sales/src/App.vue
+++ sales/src/App.vue
@@ -1,50 +1,16 @@
 <template>
   <div id="app">
-    <!-- <customer-query></customer-query> -->
-    <!-- <seller-query></seller-query> -->
-    <div v-if="isLogin">
-      <component :is="view"></component>
-    </div>
-    <div v-else>
-      <loginReg @login-success="loginSuccess"></loginReg>
-    </div>
+    <customer-query></customer-query>
   </div>
 </template>
 
 <script>
-  import buyer from './components/customer-query.vue'
-  import seller from './components/seller-query.vue'
-  import checker from './components/checker-query.vue'
-  import loginReg from './components/login&register.vue'
-
+  import customerQuery from './components/customer-query.vue'
   export default {
     methods: {},
     components: {
-      buyer: buyer,
-      seller: seller,
-      checker: checker,
-      loginReg: loginReg,
+      'customer-query': customerQuery
     },
-    data() {
-      return {
-        isLogin: false,
-        view: loginReg,
-      }
-    },
-    methods: {
-        loginSuccess(val) {
-        this.isLogin = true
-        if (val == "seller") {
-          this.view = seller
-        }
-        else if(val == "buyer") {
-          this.view = buyer
-        }
-        else if (val == "checker") {
-          this.view = checker
-        }
-      },
-    }
   }
 </script>
 
--- sales/src/main.js
+++ sales/src/main.js
@@ -2,25 +2,15 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
-import Message from 'element-ui/lib/message'
-import UIkit from 'uikit';
-import Icons from 'uikit/dist/js/uikit-icons';
-// import VueCharts from 'vue-chartjs'
+import ECharts from 'vue-echarts/components/ECharts.vue'
 
 Vue.use(ElementUI)
-Vue.use(Message)
+Vue.component('chart', ECharts)
 
 new Vue({
     el: '#app',
     render: h => h(App)
 })
 
-// new Vue({
-//     el: '#app',
-//     template: '<App/>',
-//     components: { App }
-// })
-
 //全局变量header
 Vue.prototype.urlHeader = 'http://10.60.43.111:19999'
-Vue.prototype.salesHeader = "http://10.60.43.111:16666"
